09798df3a1c53369d5cba5bb31f6023ca6698e99
Daniel Hernandez FCO <daniel@localhost.localdomain>
2020-04-15 09:47:44 -0500

    mejorado ejemplo de timbrado

--- timbrado_ejemplo_soap_getCFDI.rb
+++ timbrado_ejemplo_soap_getCFDI.rb
@@ -2,11 +2,13 @@
 
 #gem install savon
 #gem install iniparse
+#gem install rubyzip
 require 'json'
 require 'savon'
 require 'iniparse'
 require 'time'
 require "base64"
+require 'zip'
 
 config=IniParse.parse( File.read('config.ini') )
 
@@ -20,11 +22,14 @@ usuario=config['timbrado']['UsuarioSIFEI']
 
 #file=File.open('xml_sellado.xml','r');
 #cfdi_contenido=file.read
-cfdi_contenido=File.read('xml_sellado.xml')
+cfdi_contenido=File.read('assets/xml_sellado.xml')
 
 #savon 2 no ofrece una manera directa de acceder al request por lo que deberas de utilizar algun interceptor
 client = Savon.client(wsdl: 'http://devcfdi.sifei.com.mx:8080/SIFEI33/SIFEI?wsdl',
     #log: true
+    convert_request_keys_to: :none,
+    open_timeout: 300,
+    read_timeout: 300,
     )
 #print client.operations
 begin
@@ -33,12 +38,12 @@ begin
         Usuario: usuario,
         Password:password ,
         IdEquipo: idEquipo,
-        archivoXMLZip: Base64.encode64(cfdi_contenido), # 
+        archivoXMLZip: Base64.strict_encode64(cfdi_contenido), # 
         Serie:"" ,
     }
     
-    print soapParams
-    #simulamos 
+    #print soapParams
+    #simulamos request para guardarlo y ver como se envia(omitir en produccion)
     operation=client.operation(:get_cfdi)
     request= operation.build(message:soapParams).pretty
     timestamp=Time.now.to_i.to_s    
@@ -46,6 +51,19 @@ begin
     response = client.call(:get_cfdi, message:soapParams)
     # print response.to_xml
     File.open("timbrado_response_#{timestamp}.xml" ,"w"){ |file|file.puts response.to_xml}
+    puts "Escribiendo zip"
+    zipName="zip_timbrado_#{timestamp}.zip"
+    #se decodifica el resultado de la respuesta(base64) y se escribe a un archivo zip que contiene el xml timbrado
+    File.open(zipName ,"w"){ |file|file.puts Base64.strict_decode64(response.body[:get_cfdi_response][:return])}
+
+
+    #una vez escrito el zip, debemos extraer el XML timbrado
+    Zip::File.open(zipName) do |zipfile|
+        zipfile.each do |file|
+          # do something with file
+          file.extract
+        end
+      end
 rescue Savon::SOAPFault => e  
     #print client.request
     puts "Exception:"
